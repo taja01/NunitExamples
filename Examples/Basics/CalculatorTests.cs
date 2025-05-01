eb8a84a0ae80fe1d9ad3bbc3fcfbefc01ff65749
taja01 <jarvas.tamas@gmail.com>
2025-05-01 22:06:45 +0200

    first implementation

---
+++ Examples/Basics/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Basics
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_WhenCalled_ReturnsSumOfArguments()
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            int a = 5, b = 7;
+
+            // Act
+            int result = calculator.Add(a, b);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(12), "Addition did not return expected result.");
+        }
+    }
+}
