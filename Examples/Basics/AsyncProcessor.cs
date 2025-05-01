cad77eec7e191d19465abed99f2a1d8c318bf273
taja01 <jarvas.tamas@gmail.com>
2025-05-01 22:12:29 +0200

    async test add

---
+++ Examples/Basics/AsyncProcessor.cs
@@ -0,0 +1,13 @@
+﻿namespace Basics
+{
+    public class AsyncProcessor
+    {
+        public async Task<string> ProcessDataAsync()
+        {
+            await Task.Delay(100);
+
+            // Simulate asynchronous work
+            return "Processed";
+        }
+    }
+}
---
+++ Examples/Basics/AsyncProcessorTests.cs
@@ -0,0 +1,14 @@
+﻿namespace Basics
+{
+    [TestFixture]
+    public class AsyncProcessorTests
+    {
+        [Test]
+        public async Task ProcessDataAsync_ReturnsProcessed()
+        {
+            var processor = new AsyncProcessor();
+            string result = await processor.ProcessDataAsync();
+            Assert.That(result, Is.EqualTo("Processed"));
+        }
+    }
+}
