195edcbdbb32c861f810122f073bf8cf8ed226df
taja01 <jarvas.tamas@gmail.com>
2025-05-02 21:32:21 +0200

    next example: petstore adding client project

---
+++ Examples/PetStore/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PetStore
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddWeatherAppClient(this IServiceCollection services, IConfiguration configuration)
+        {
+            var options = new PetStoreOptions();
+            configuration.Bind(nameof(PetStoreOptions), options);
+            services.Configure<PetStoreOptions>(configuration.GetSection(nameof(PetStoreOptions)));
+
+            services.AddHttpClient<IPetStoreClient, PetStoreClient>(c => c.BaseAddress = new Uri(options.BaseAddress));
+            return services;
+        }
+    }
+}
