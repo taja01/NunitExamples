b1f48a6408f0e8b2a5e6bcf7fb3f8845b039727f
taja01 <jarvas.tamas@gmail.com>
2025-05-05 23:49:04 +0200

    Data-Driven Testing

---
+++ Examples/Basics/DataDrivenTests.cs
@@ -0,0 +1,18 @@
+﻿namespace Basics;
+
+[TestFixture]
+public class DataDrivenTests
+{
+    static object[] TestCases =
+    {
+        new object[] { 1, 2, 3 },
+        new object[] { -1, -1, -2 },
+        new object[] { 0, 0, 0 }
+    };
+
+    [TestCaseSource(nameof(TestCases))]
+    public void AddTest(int a, int b, int expected)
+    {
+        Assert.That(a + b, Is.EqualTo(expected));
+    }
+}
