5aa4145e1a878f521e7314a2b014ee5a7b14f806
taja01 <jarvas.tamas@gmail.com>
2025-05-05 23:45:22 +0200

    Customizing Test Names

---
+++ Examples/Basics/CustomizingTestNames.cs
@@ -0,0 +1,12 @@
+﻿namespace Basics;
+
+[TestFixture]
+public class CustomizingTestNames
+{
+    [TestCase(1, 2, 3, TestName = "Addition_When1and2_Returns3")]
+    [TestCase(2, 5, 7, TestName = "Addition_When2and5_Returns7")]
+    public void TestAddition(int a, int b, int expected)
+    {
+        Assert.That(a + b, Is.EqualTo(expected));
+    }
+}
