0a9ceb3405aedb09556a4ec264a0b0645b92a78d
taja01 <jarvas.tamas@gmail.com>
2025-05-05 23:43:37 +0200

    Parameterized Test Fixtures

---
+++ Examples/Basics/CrossPlatformTests.cs
@@ -0,0 +1,21 @@
+﻿namespace Basics;
+
+[TestFixture("windows")]
+[TestFixture("linux")]
+[TestFixture("macos")]
+public class CrossPlatformTests
+{
+    private readonly string _os;
+
+    public CrossPlatformTests(string os)
+    {
+        _os = os;
+    }
+
+    [Test]
+    public void TestOperatingSystem()
+    {
+        Assert.That(_os, Is.Not.Null);
+        TestContext.WriteLine($"Testing on {_os}");
+    }
+}
