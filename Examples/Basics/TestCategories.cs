7fb10eece4c2492712def0e90eade39e375c5bf8
taja01 <jarvas.tamas@gmail.com>
2025-05-05 23:47:55 +0200

    Test Categories

---
+++ Examples/Basics/TestCategories.cs
@@ -0,0 +1,19 @@
+﻿namespace Basics;
+
+public class TestCategories
+{
+    [Test]
+    [Category("Integration")]
+    public void IntegrationTest()
+    {
+        Assert.Pass("This is an integration test.");
+    }
+
+    [Test]
+    [Category("Unit")]
+    public void UnitTest()
+    {
+        Assert.Pass("This is a unit test.");
+    }
+}
+//--where "cat==Integration"
